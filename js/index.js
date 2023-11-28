8ec77e58d7dc9334b7d6af4219f70a351b1e0778
lucbac06 <lucas.backdahl@gmail.com>
2023-11-28 10:39:03 +0100

    Started on Material You theming, imported colors and some objects to use on the website

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// index.js
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/elevation/elevation'
+import '@material/web/checkbox/checkbox.js';
